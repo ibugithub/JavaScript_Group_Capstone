748c6afe7b0a7566425fc697d76d345b18e58bb3
Moyasi Ginko <mrsiyam61@gmail.com>
2023-05-30 02:26:24 +0600

    Add page dependencies

---
+++ dist/index.bundle.js
@@ -0,0 +1,18 @@
+(self["webpackChunkjavascript_group_capstone"] = self["webpackChunkjavascript_group_capstone"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
