773213eadb373e4af5be2cb56dff7cc0b3c9826d
ibugithub <outsideworkibrahim@gmail.com>
2023-05-30 08:44:22 +0600

    Add functionality to get data from api and make the page responsive

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
+import tvShowApi from './modules/api.js';
 import './style.css';
+import love from './assets/love.png';
+
+const mainElement = document.querySelector('.display-items');
+
+const getData = async () => {
+  const data = await tvShowApi.gettingData();
+  data.forEach((element) => {
+    try {
+      if (element.show.image.original !== null) {
+        const container = ` 
+        <div class="items">
+            <img src="${element.show.image.original}" alt="#" />
+            <h2>${element.show.name}</h2>
+            <div class="likes_love">
+                <span>Likes</span>
+                <img src="${love}">
+            </div>
+            <div class="comment_reserve"> 
+                <button type="button" class="cmnt">Comments</button>
+                <button type="button" class="rsrv">Reservation</button>
+            </div>
+        </div>
+        `;
+        mainElement.insertAdjacentHTML('beforeend', container);
+      }
+    } catch (error) {
+      return 0;
+    }
+    return 1;
+  });
+};
+
+getData();
