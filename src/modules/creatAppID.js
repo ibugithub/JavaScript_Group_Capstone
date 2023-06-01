e7065c718366aae352a7cd6bea9a74ee60ca24df
Moyasi Ginko <mrsiyam61@gmail.com>
2023-06-01 10:38:29 +0600

    Create appID function to retrieve id

---
+++ src/modules/creatAppID.js
@@ -0,0 +1,16 @@
+async function createApp() {
+  try {
+    const response = await fetch(
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/',
+      {
+        method: 'POST',
+      }
+    );
+    const data = await response.text();
+    console.log('App created successfully. App ID:', data);
+  } catch (error) {
+    console.error('Error creating app:', error);
+  }
+}
+
+createApp();
