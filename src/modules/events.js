c45c843acae8b6fb446302136b91861d6872f8e7
ibugithub <outsideworkibrahim@gmail.com>
2023-06-02 08:28:37 +0600

    Fix merge miss into dev from display-likes

--- src/modules/events.js
+++ src/modules/events.js
@@ -7,32 +7,36 @@ class EventsHandler {
     onPageLoad = async () => {
       const mainElement = document.querySelector('.display-items');
       const data = await tvShowApi.gettingData();
+      const appId = 'jDmx1fPvHr5KpfZ0L9Bv';
       try {
-        data.forEach((element) => {
+        data.forEach(async (element) => {
           if (element.show.image.original !== null) {
-            const container = ` 
+            const { id, name, image } = element.show;
+            const count = await tvShowApi.fetchItemLikes(appId, id);
+            const container = `
               <div class="items">
-                  <img src="${element.show.image.original}" alt="#" />
-                  <h2>${element.show.name}</h2>
-                  <div class="likes_love">
-                      <span>Likes</span>
-                      <img src="${love}">
-                  </div>
-                  <div class="comment_reserve"> 
-                      <button type="button" data-id=${element.show.id} class="cmnt">Comments</button>
-                      <button type="button" class="rsrv">Reservation</button>
-                  </div>
+                <img src="${image.original}" alt="#" />
+                <h2>${name}</h2>
+                <div class="likes_love">
+                  <span>Likes: ${count}</span>
+                  <button class="love" id="love-${id}"><img src="${love}"></button>
+                </div>
+                <div class="comment_reserve">
+                  <button type="button" data-id="${id}" class="cmnt">Comments</button>
+                  <button type="button" class="rsrv">Reservation</button>
+                </div>
               </div>
-              `;
+            `;
+
             mainElement.insertAdjacentHTML('beforeend', container);
           }
         });
-        this.handleCommentBtnClick();
+
         return 1;
       } catch (error) {
         return error;
       }
-    }
+    };
 }
 
 const eventhandler = new EventsHandler();
