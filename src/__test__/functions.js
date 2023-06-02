e101d454659fe566536a79d1b8da50c4fd85c8f7
Moyasi Ginko <mrsiyam61@gmail.com>
2023-06-02 22:05:01 +0600

    Remove unused code and fix linters errors

--- src/__test__/functions.js
+++ src/__test__/functions.js
@@ -1,43 +1,7 @@
-/*eslint-disable*/
-const getAppId = () => {
-  const baseUrl =
-    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
-  const endpoint = '/apps/';
-
-  fetch(baseUrl + endpoint, {
-    method: 'POST',
-  })
-    .then((response) => response.text())
-    .then((appId) => {
-      console.log('Created app with ID:', appId);
-    });
-};
-
-const postData = () => {
-  fetch(
-    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/jDmx1fPvHr5KpfZ0L9Bv/likes',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ item_id: 9311 }),
-    }
-  ).then((response) => console.log(response));
-};
-
-const getData = () => {
-  fetch(
-    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/jDmx1fPvHr5KpfZ0L9Bv/likes'
-  )
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-};
-
 export const countLove = (event) => {
   const currentCount = parseInt(
     event.target.closest('.likes_love').querySelector('.innerSpan').textContent,
-    10
+    10,
   );
   console.log('The current count is', currentCount);
   const countContainer = event.target
@@ -51,7 +15,3 @@ export const countItems = () => {
   const itemsCount = document.querySelectorAll('.items').length;
   return itemsCount;
 };
-
-// getAppId();
-// postData();
-// getData();
