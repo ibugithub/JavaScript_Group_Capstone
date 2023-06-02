9abcbfc86f1f61cabdc059aaeed0043968bafc4b
Moyasi Ginko <mrsiyam61@gmail.com>
2023-06-02 13:21:43 +0600

    Add a method to count the number of items

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,12 +1,23 @@
 class Count {
-    countLove = (event) => {
-      const currentCount = parseInt(event.target.closest('.likes_love').querySelector('.innerSpan').textContent, 10);
-      console.log('the current count is', currentCount, typeof (currentCount));
-      const countContainer = event.target.closest('.likes_love').querySelector('.innerSpan');
-      const newCounter = currentCount + 1;
-      countContainer.innerHTML = newCounter;
-    }
+  countLove = (event) => {
+    const currentCount = parseInt(
+      event.target.closest('.likes_love').querySelector('.innerSpan')
+        .textContent,
+      10,
+    );
+    console.log('the current count is', currentCount, typeof currentCount);
+    const countContainer = event.target
+      .closest('.likes_love')
+      .querySelector('.innerSpan');
+    const newCounter = currentCount + 1;
+    countContainer.innerHTML = newCounter;
+  };
+
+  countItems = () => {
+    const itemsCount = document.querySelectorAll('.items').length;
+    return itemsCount;
+  };
 }
 
-const count = new Count();
-export default count;
+const counter = new Count();
+export default counter;
