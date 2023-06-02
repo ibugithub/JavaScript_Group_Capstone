de711c31319151153e0fcc4c7845712e63cfb2b5
Moyasi Ginko <mrsiyam61@gmail.com>
2023-06-02 17:52:05 +0600

    Add functions to test

--- src/__test__/functions.js
+++ src/__test__/functions.js
@@ -34,25 +34,24 @@ const getData = () => {
     .then((data) => console.log(data));
 };
 
-const countLove = (event) => {
-    const currentCount = parseInt(
-      event.target.closest('.likes_love').querySelector('.innerSpan')
-        .textContent,
-      10
-    );
-    console.log('the current count is', currentCount, typeof currentCount);
-    const countContainer = event.target
-      .closest('.likes_love')
-      .querySelector('.innerSpan');
-    const newCounter = currentCount + 1;
-    countContainer.innerHTML = newCounter;
-  };
+export const countLove = (event) => {
+  const currentCount = parseInt(
+    event.target.closest('.likes_love').querySelector('.innerSpan').textContent,
+    10
+  );
+  console.log('The current count is', currentCount);
+  const countContainer = event.target
+    .closest('.likes_love')
+    .querySelector('.innerSpan');
+  const newCounter = currentCount + 1;
+  countContainer.innerHTML = newCounter.toString();
+};
 
-const countItems = () => {
-    const itemsCount = document.querySelectorAll('.items').length;
-    return itemsCount;
-  };
+export const countItems = () => {
+  const itemsCount = document.querySelectorAll('.items').length;
+  return itemsCount;
+};
 
 // getAppId();
 // postData();
-getData();
+// getData();
